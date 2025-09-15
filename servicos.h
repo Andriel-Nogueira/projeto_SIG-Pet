89e85ca57d42944c3cfec132e18f2414be1420d7
Andriel-Nogueiragit config --local user.email andriel.pereiraufrn@gmail.comgit config --local user.name Andriel-Nogueira <andriel.pereiraufrn@gmail.com>
2025-09-15 16:11:20 -0300

    Modularização serviços

---
+++ servicos.h
@@ -0,0 +1,12 @@
+#ifndef SERVICOS_H
+#define SERVICOS_H
+
+
+void m_servicos(void);
+void cadastrar_servico(void);
+void buscar_servico(void);
+void atualizar_servico(void);
+void listar_servicos(void);
+void excluir_servico(void);
+
+#endif
