d51d05fa5d97e6ef511bc277e0e3074065c909c1
Andriel-Nogueiragit config --local user.email andriel.pereiraufrn@gmail.comgit config --local user.name Andriel-Nogueira <andriel.pereiraufrn@gmail.com>
2025-09-15 16:27:37 -0300

    Modularização Produtos

--- produtos.c
+++ produtos.c
@@ -1,82 +1,10 @@
-//////////////////////////////////////////////////////////////////////////////////////////////////
-//    ,-,--.    .=-.-.       _,---.                        _ __         ,----.   ,--.--------. ///
-//  ,-.'-  _\  /==/_ /   _.='.'-,  \                    .-`.' ,`.    ,-.--` , \ /==/,  -   , -\ //
-// /==/_ ,_.' |==|, |   /==.'-     /   ,--.--------.   /==/, -   \  |==|-  _.-` \==\.-.  - ,-./ //
-// \==\  \    |==|  |  /==/ -   .-'   /==/,  -   , -\ |==| _ .=. |  |==|   `.-.  `--`\==\- | /////
-//  \==\ -\   |==|- |  |==|_   /_,-.  \==\.-.  - ,-./ |==| , '=',| /==/_ ,    /       \==\_ | ////
-//  _\==\ ,\  |==| ,|  |==|  , \_.' )  `--`--------`  |==|-  '..'  |==|    .-'        |==|- | ////
-// /==/\/ _ | |==|- |  \==\-  ,    (                  |==|,  |     |==|_  ,`-._       |==|, | ////
-// \==\ - , / /==/. /   /==/ _  ,  /                  /==/ - |     /==/ ,     /       /==/ -/ ////
-//  `--`---'  `--`-`    `--`------'                   `--`---'     `--`-----``        `--`--` ////
-//////////////////////////////////////////////////////////////////////////////////////////////////
-///                       Universidade Federal do Rio Grande do Norte                          ///
-///                          Centro de Ensino Superior do Seridó                               ///
-///                            Disciplina DCT1106 -- Programação                               ///
-///                        Projeto Sistema de Atendimento de Pet Shop                          ///
-///              Developed by @Andriel-Nogueira & @Carlos-Henrique-de-Oliveira-Batista         ///
-//////////////////////////////////////////////////////////////////////////////////////////////////
-///                                         Semana 1                                           ///
-//////////////////////////////////////////////////////////////////////////////////////////////////
-
 #include <stdio.h>
 #include <stdlib.h>
-#include "clientes.h"
-#include "servicos.h"
-#include "agendamentos.h"
 #include "produtos.h"
 
-// Funções Principais
-void m_principal(void);
-void t_sobre(void);
-void t_equipe(void);
-
-void t_final(void);
-
-//Programa Principal
-int main(void) {
-    int op; 
-    system("clear");
-    do {
-        system("clear");
-        printf("\n");
-    printf("╔═════════════════════════════════════╗\n");
-    printf("║           Sistema SIG-Pet           ║\n");
-    printf("╠═════════════════════════════════════╣\n");
-    printf("║ 1 - Menu Principal                  ║\n");
-    printf("║ 2 - Sobre o sistema                 ║\n");
-    printf("║ 3 - Equipe de desenvolvimento       ║\n");
-    printf("║ 0 - Sair                            ║\n");
-    printf("╚═════════════════════════════════════╝\n");
-    printf("\n");
-    printf("Escolha a opção desejada:\n");
-    scanf(" %d", &op);
-    getchar();
-    switch(op){
-        case 1:
-            m_principal();
-            break;
-        case 2:
-            t_sobre();
-            break;
-        case 3:
-            t_equipe();
-            break;
-        case 0:
-            t_final();
-            break;
-        default:
-            printf("Opção inválida. Tente novamente.\n");
-        }   
-    } while(op != 0);
-    
-    
-    return 0;
-}
-
-void m_principal(void) {
+void m_produtos(void) {
     int op;
     system("clear");
-
     do {
         system("clear");
         printf("\n");
@@ -92,16 +20,15 @@ void m_principal(void) {
     printf("║ \\==\\ - , / /==/. /   /==/ _  ,  /                  /==/ - |     /==/ ,     /       /==/ -/   ║\n");
     printf("║  `--`---'  `--`-`    `--`------'                   `--`---'     `--`-----``        `--`--`   ║\n");
     printf("╠══════════════════════════════════════════════════════════════════════════════════════════════╣\n");
-    printf("║                                         Semana 1                                             ║\n");
-    printf("╠══════════════════════════════════════════════════════════════════════════════════════════════╣\n");
+    printf("║                                   Menu Produtos                                              ║\n");
+    printf("╠══════════════════════════════════════════════════════════════════════════════════════════════║/n");
     printf("║                                                                                              ║\n");
-    printf("║          1 - Módulo Clientes                                                                 ║\n");
-    printf("║          2 - Módulo Serviços                                                                 ║\n");
-    printf("║          3 - Módulo Agendamentos                                                             ║\n");
-    printf("║          4 - Módulo Produtos                                                                 ║\n");
-    printf("║          5 - Módulo Vendas                                                                   ║\n");
-    printf("║          6 - Módulo Relatórios                                                               ║\n");
-    printf("║          0 - Sair                                                                            ║\n");
+    printf("║          1 - Adicionar novo produto                                                          ║\n");
+    printf("║          2 - Buscar produto pelo código                                                      ║\n");
+    printf("║          3 - Atualizar produto                                                               ║\n");
+    printf("║          4 - Listar produtos                                                                 ║\n");
+    printf("║          5 - Excluir produto                                                                 ║\n");
+    printf("║          0 - Voltar ao menu principal                                                        ║\n");
     printf("║                                                                                              ║\n");
     printf("║          Escolha uma opção:                                                                  ║\n");
     printf("╚══════════════════════════════════════════════════════════════════════════════════════════════╝\n");
@@ -110,16 +37,19 @@ void m_principal(void) {
     getchar();
     switch(op){
         case 1:
-            m_clientes();
+            adicionar_produto();
             break;
         case 2:
-            m_servicos();
+            buscar_produto();
             break;
         case 3:
-            m_agendamento();
+            atualizar_produto();
             break;
         case 4:
-            m_produtos();
+            listar_produtos();
+            break;
+        case 5:
+            excluir_produto();
             break;
         case 0:
             break;
@@ -127,10 +57,9 @@ void m_principal(void) {
             printf("Opção inválida. Tente novamente.\n");
         }
     } while(op != 0);
-    
 }
 
-void t_sobre(void) {
+void adicionar_produto(void) {
     system("clear");
     printf("\n");
     printf("╔══════════════════════════════════════════════════════════════════════════════════════════════╗\n");
@@ -144,29 +73,22 @@ void t_sobre(void) {
     printf("║ \\==\\ - , / /==/. /   /==/ _  ,  /                  /==/ - |     /==/ ,     /       /==/ -/   ║\n");
     printf("║  `--`---'  `--`-`    `--`------'                   `--`---'     `--`-----``        `--`--`   ║\n");
     printf("╠══════════════════════════════════════════════════════════════════════════════════════════════╣\n");
-    printf("║                       Universidade Federal do Rio Grande do Norte                            ║\n");
-    printf("║                          Centro de Ensino Superior do Seridó                                 ║\n");
-    printf("║                            Disciplina DCT1106 -- Programação                                 ║\n");
-    printf("║                        Projeto Sistema de Atendimento de Pet Shop                            ║\n");
-    printf("║              Developed by @Andriel-Nogueira & @Carlos-Henrique-de-Oliveira-Batista           ║\n");
-    printf("╠══════════════════════════════════════════════════════════════════════════════════════════════╣\n");
-    printf("║                                         Semana 1                                             ║\n");
+    printf("║                                  Adicionar Produto                                           ║\n");
     printf("╠══════════════════════════════════════════════════════════════════════════════════════════════╣\n");
     printf("║                                                                                              ║\n");
-    printf("║          Este é um sistema de atendimento para Pet Shop, desenvolvido como parte de uma      ║\n");
-    printf("║          atividade acadêmica. O sistema é dividido em módulos que permitem o gerenciamento   ║\n");
-    printf("║          de clientes, serviços, produtos, vendas, estoque e relatórios.                      ║\n");
-    printf("║          O objetivo é proporcionar uma experiência de aprendizado prática e funcional,       ║\n");
-    printf("║          permitindo que os usuários gerenciem eficientemente as operações de um Pet Shop.    ║\n");
+    printf("║      Nome do Produto:                                                                        ║\n");
+    printf("║      Descrição:                                                                              ║\n");
+    printf("║      Preço:                                                                                  ║\n");
+    printf("║      Quantidade em Estoque:                                                                  ║\n");
+    printf("║      Código do Produto:                                                                      ║\n");
     printf("║                                                                                              ║\n");
     printf("╚══════════════════════════════════════════════════════════════════════════════════════════════╝\n");
     printf("\n");
-    printf(                        "Pressione <Enter> para voltar ao menu principal...\n");
+    printf(                     "Pressione <Enter> para voltar ao menu principal...                         \n");
     getchar();
-    
 }
 
-void t_equipe(void) {
+void buscar_produto(void) {
     system("clear");
     printf("\n");
     printf("╔══════════════════════════════════════════════════════════════════════════════════════════════╗\n");
@@ -180,25 +102,18 @@ void t_equipe(void) {
     printf("║ \\==\\ - , / /==/. /   /==/ _  ,  /                  /==/ - |     /==/ ,     /       /==/ -/   ║\n");
     printf("║  `--`---'  `--`-`    `--`------'                   `--`---'     `--`-----``        `--`--`   ║\n");
     printf("╠══════════════════════════════════════════════════════════════════════════════════════════════╣\n");
-    printf("║                                         Semana 1                                             ║\n");
+    printf("║                             Buscar Produto pelo Código                                       ║\n");
     printf("╠══════════════════════════════════════════════════════════════════════════════════════════════╣\n");
     printf("║                                                                                              ║\n");
-    printf("║          Equipe de Desenvolvimento:                                                          ║\n");
-    printf("║          - Andriel Nogueira                                                                  ║\n");
-    printf("║            Email: andriel.pereiraufrn@gmail.com                                              ║\n");
-    printf("║            GitHub: @Andriel-Nogueira                                                         ║\n");
-    printf("║          - Carlos Henrique de Oliveira Batista                                               ║\n");
-    printf("║            Email: carlos.oliveira.714@ufrn.com.br                                            ║\n");
-    printf("║            GitHub: @Carlos-Henrique-de-Oliveira-Batista                                      ║\n");
-    printf("║          - Repositório da Equipe: https://github.com/Andriel-Nogueira/projeto_SIG-Pet.git    ║\n");
+    printf("║      Informe o Código do Produto:                                                            ║\n");
+    printf("║                                                                                              ║\n");
     printf("╚══════════════════════════════════════════════════════════════════════════════════════════════╝\n");
     printf("\n");
-    printf(                        "Pressione <Enter> para voltar ao menu principal...\n");
+    printf(                     "Pressione <Enter> para voltar ao menu principal...                         \n");
     getchar();
-    
 }
 
-void t_final(void) {
+void atualizar_produto(void) {
     system("clear");
     printf("\n");
     printf("╔══════════════════════════════════════════════════════════════════════════════════════════════╗\n");
@@ -212,15 +127,63 @@ void t_final(void) {
     printf("║ \\==\\ - , / /==/. /   /==/ _  ,  /                  /==/ - |     /==/ ,     /       /==/ -/   ║\n");
     printf("║  `--`---'  `--`-`    `--`------'                   `--`---'     `--`-----``        `--`--`   ║\n");
     printf("╠══════════════════════════════════════════════════════════════════════════════════════════════╣\n");
-    printf("║                                         Semana 1                                             ║\n");
+    printf("║                                 Atualizar Produto                                            ║\n");
     printf("╠══════════════════════════════════════════════════════════════════════════════════════════════╣\n");
     printf("║                                                                                              ║\n");
-    printf("║          Obrigado por utilizar o Sistema SIG-Pet!                                            ║\n");
-    printf("║          Esperamos ter proporcionado uma ótima experiência!                                  ║\n");
-    printf("║          Volte Sempre!                                                                       ║\n");
-    printf("║            __    __                                                ||      _,,,---,,_        ║\n");
-    printf("║          o-''))_____\\                                        ZzZzz /,`.-'`'    -.  ;-;;,_    ║\n");
-    printf("║          z--__/ * * * )                                           |,4-  ) )-,_. ,/ (  `'-'   ║\n");
-    printf("║          c_c__/-c____/                                           '---''(_/--'  `-'/_)        ║\n");
+    printf("║      Informe o Código do Produto que deseja atualizar:                                       ║\n");
+    printf("║                                                                                              ║\n");
     printf("╚══════════════════════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("\n");
+    printf(                     "Pressione <Enter> para voltar ao menu principal...                         \n");
+    getchar();
+}
+
+void listar_produtos(void) {
+    system("clear");
+    printf("\n");
+    printf("╔══════════════════════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║    ,-,--.    .=-.-.       _,---.                        _ __         ,----.   ,--.--------.  ║\n");
+    printf("║  ,-.'-  _\\  /==/_ /   _.='.'-,  \\                    .-`.' ,`.    ,-.--` , \\ /==/,  -   , -  ║\n");
+    printf("║ /==/_ ,_.' |==|, |   /==.'-     /   ,--.--------.   /==/, -   \\  |==|-  _.-` \\==\\.-.  - ,-.  ║\n");
+    printf("║ \\==\\  \\    |==|  |  /==/ -   .-'   /==/,  -   , -\\ |==| _ .=. |  |==|   `.-.  `--`\\==\\-  |   ║\n");
+    printf("║  \\==\\ -\\   |==|- |  |==|_   /_,-.  \\==\\.-.  - ,-./ |==| , '=',| /==/_ ,    /       \\==\\_ |   ║\n");
+    printf("║  _\\==\\ ,\\  |==| ,|  |==|  , \\_.' )  `--`--------`  |==|-  '..'  |==|    .-'        |==|- |   ║\n");
+    printf("║ /==/\\/ _ | |==|- |  \\==\\-  ,    (                  |==|,  |     |==|_  ,`-._       |==|, |   ║\n");
+    printf("║ \\==\\ - , / /==/. /   /==/ _  ,  /                  /==/ - |     /==/ ,     /       /==/ -/   ║\n");
+    printf("║  `--`---'  `--`-`    `--`------'                   `--`---'     `--`-----``        `--`--`   ║\n");
+    printf("╠══════════════════════════════════════════════════════════════════════════════════════════════╣\n");
+    printf("║                                   Listar Produtos                                            ║\n");
+    printf("╠══════════════════════════════════════════════════════════════════════════════════════════════╣\n");
+    printf("║                                                                                              ║\n");
+    printf("║      Lista de Produtos:                                                                      ║\n");
+    printf("║                                                                                              ║\n");
+    printf("╚══════════════════════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("\n");
+    printf(                     "Pressione <Enter> para voltar ao menu principal...                         \n");
+    getchar();
+}
+
+void excluir_produto(void) {
+    system("clear");
+    printf("\n");
+    printf("╔══════════════════════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║    ,-,--.    .=-.-.       _,---.                        _ __         ,----.   ,--.--------.  ║\n");
+    printf("║  ,-.'-  _\\  /==/_ /   _.='.'-,  \\                    .-`.' ,`.    ,-.--` , \\ /==/,  -   , -  ║\n");
+    printf("║ /==/_ ,_.' |==|, |   /==.'-     /   ,--.--------.   /==/, -   \\  |==|-  _.-` \\==\\.-.  - ,-.  ║\n");
+    printf("║ \\==\\  \\    |==|  |  /==/ -   .-'   /==/,  -   , -\\ |==| _ .=. |  |==|   `.-.  `--`\\==\\-  |   ║\n");
+    printf("║  \\==\\ -\\   |==|- |  |==|_   /_,-.  \\==\\.-.  - ,-./ |==| , '=',| /==/_ ,    /       \\==\\_ |   ║\n");
+    printf("║  _\\==\\ ,\\  |==| ,|  |==|  , \\_.' )  `--`--------`  |==|-  '..'  |==|    .-'        |==|- |   ║\n");
+    printf("║ /==/\\/ _ | |==|- |  \\==\\-  ,    (                  |==|,  |     |==|_  ,`-._       |==|, |   ║\n");
+    printf("║ \\==\\ - , / /==/. /   /==/ _  ,  /                  /==/ - |     /==/ ,     /       /==/ -/   ║\n");
+    printf("║  `--`---'  `--`-`    `--`------'                   `--`---'     `--`-----``        `--`--`   ║\n");
+    printf("╠══════════════════════════════════════════════════════════════════════════════════════════════╣\n");
+    printf("║                                   Excluir Produto                                            ║\n");
+    printf("╠══════════════════════════════════════════════════════════════════════════════════════════════╣\n");
+    printf("║                                                                                              ║\n");
+    printf("║      Informe o Código do Produto que deseja excluir:                                         ║\n");
+    printf("║                                                                                              ║\n");
+    printf("╚══════════════════════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("\n");
+    printf(                     "Pressione <Enter> para voltar ao menu principal...                         \n");
+    getchar();
 }
---
+++ produtos.h
@@ -0,0 +1,11 @@
+#ifndef PRODUTOS_H
+#define PRODUTOS_H
+
+void m_produtos(void);
+void adicionar_produto(void);
+void buscar_produto(void);
+void atualizar_produto(void);
+void listar_produtos(void);
+void excluir_produto(void);
+
+#endif
