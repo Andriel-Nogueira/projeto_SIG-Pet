1a9044c15ead7794453c848ad7a976518a7204f6
Andriel-Nogueiragit config --local user.email andriel.pereiraufrn@gmail.comgit config --local user.name Andriel-Nogueira <andriel.pereiraufrn@gmail.com>
2025-09-15 16:23:35 -0300

    Modularização Agendamentos

---
+++ agendamentos.h
@@ -0,0 +1,11 @@
+#ifndef AGENDAMENTOS_H
+#define AGENDAMENTOS_H
+
+void m_agendamento(void);
+void agendar(void);
+void buscar_agend(void);
+void atualizar_agend(void);
+void listar_agend(void);
+void excluir_agend(void);
+
+#endif
