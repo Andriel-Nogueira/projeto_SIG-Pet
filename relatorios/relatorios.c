2e2fd8853033669bf210a26406dc2652ac851b63
JaineSouz <jainesouza1520@gmail.com>
2025-11-12 01:11:44 -0300

    Refactor: Implementando m_relatorios para ter sub-menus

--- relatorios/relatorios.c
+++ relatorios/relatorios.c
@@ -19,11 +19,12 @@ void m_relatorios(void)
         exibir_titulo("Menu Relatórios");
 
         printf("║                                                                                              ║\n");
-        printf("║          1 - Listagem geral de clientes                                                      ║\n");
-        printf("║          2 - Listagem geral de produtos                                                      ║\n");
-        printf("║          3 - Listagem geral de serviços                                                      ║\n");
-        printf("║          4 - Listagem geral de vendas                                                        ║\n");
-        printf("║          5 - Listagem geral de agendamentos                                                  ║\n");
+        printf("║          1 - Relatórios de Clientes                                                          ║\n");
+        printf("║          2 - Relatórios de Pets                                                              ║\n");
+        printf("║          3 - Relatórios de Produtos                                                          ║\n");
+        printf("║          4 - Relatórios de Serviços                                                          ║\n");
+        printf("║          5 - Relatórios de Vendas                                                            ║\n");
+        printf("║          6 - Relatórios de Agendamentos                                                      ║\n");
         printf("║          0 - Voltar ao menu principal                                                        ║\n");
         printf("║                                                                                              ║\n");
         printf("║          Escolha uma opção:                                                                  ║\n");
@@ -34,19 +35,22 @@ void m_relatorios(void)
         switch (op)
         {
         case 1:
-            listar_clientes();
+            relatorio_clientes();
             break;
         case 2:
-            listar_produtos();
+            relatorio_pets();
             break;
         case 3:
-            listar_servicos();
+            relatorio_produtos();
             break;
         case 4:
-            listar_vendas();
+            relatorio_servicos();
             break;
         case 5:
-            listar_agend();
+            relatorio_vendas();
+            break;
+        case 6:
+            relatorio_agendamentos();
             break;
         case 0:
             break;
